26f99f2a49d5cead85cf70844d0aca658d0fca4c
rodrigodornelles <rodrigo.dornelles.poa@gmail.com>
2025-04-27 21:28:33 -0300

    fix: correct calendar

--- src/service/TemperatureDay.lua
+++ src/service/TemperatureDay.lua
@@ -1,13 +1,13 @@
 local TemperatureDay = {}
 
 function TemperatureDay.on_weather_api_response_day(std, data, api)
-    local index, count = 1, 0
+    local index, count = 1, 1
     local timestamp = os.time()
 
-    while index <= #api.hourly.time and count <= 7 do
-        if true then
-            local key_3 = string.format('put_%d_3_calendar', index)
-            local key_2 = string.format('put_%d_2_calendar', index)
+    while index <= #api.hourly.time and count <= 8 do
+        if api.hourly.time[index] > timestamp then
+            local key_3 = string.format('put_%d_3_calendar', count)
+            local key_2 = string.format('put_%d_2_calendar', count)
             local val_3 = string.format('%.0f \194\186C', api.hourly.temperature_2m[index])
             local val_2 = os.date("%H:%M", api.hourly.time[index])
             std.bus.emit(key_3, val_3)
