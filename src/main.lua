359a4ddd00a3c44434984c479f49f39cf0e04249
João Vicente <joao.vicente@zedia.com.br>
2025-05-21 19:21:53 -0300

    feat: add a background image

--- src/main.lua
+++ src/main.lua
@@ -23,6 +23,7 @@ App.fonts = {
 
 App.assets = {
     'assets/clean-day.jpg:clean-day.jpg',
+    'assets/opening_background.png:assets/opening_background.png',
     'assets/Pe-icon-7-weather.ttf:Pe-icon-7-weather.ttf'
 }
 
