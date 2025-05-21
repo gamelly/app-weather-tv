f84497b73b7ccd91ad8f7e192baadf84cd82ff54
João Vicente <joao.vicente@zedia.com.br>
2025-05-21 19:23:41 -0300

    refactor: remove extra rects and adjust font size

--- src/ui/elements/TextOutline.lua
+++ src/ui/elements/TextOutline.lua
@@ -12,16 +12,12 @@ function Text.draw(std, data)
         data.padding = 0
     end
     local p2 = data.padding + data.padding
-    std.text.font_size(18)
+    std.text.font_size(22)
     if not data.label_width then
         data.label_width, data.label_height = std.text.mensure(data.label)
     end
     std.draw.color(std.color.white)
-    std.draw.rect(0, 0, 0, data.label_width + p2, data.label_height + p2)
-    std.draw.color(std.color.lightgray)
-    std.draw.rect(1, 0, 0, data.label_width + p2, data.label_height + p2)
-    std.draw.color(std.color.darkgray)
-    std.text.print(data.padding, data.padding, data.label)
+    std.text.print((data.width / 2) - (data.label_width / 2), 2*data.padding, data.label)
 end
 
 return Text
