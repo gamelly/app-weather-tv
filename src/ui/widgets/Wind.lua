0225c84ac8e90587856f258c87a0576c8eb81886
João Vicente <joao.vicente@zedia.com.br>
2025-05-22 18:19:07 -0300

    refactor: change wind icon and add a text bellow

--- src/ui/widgets/Wind.lua
+++ src/ui/widgets/Wind.lua
@@ -1,16 +1,20 @@
 local Wind = {}
 
 function Wind.draw(std, data)
-    std.draw.color(0x33333399)
-    std.draw.rect(0, 0, 0, data.width, data.height)
+    -- std.draw.color(0x33333399)
+    -- std.draw.rect(0, 0, 0, data.width, data.height)
 
     std.draw.color(std.color.white)
     std.text.font_name('Weather')
     std.text.font_size(128)
-    local text = '\238\152\180'
+    local text = '\238\152\136'
     local w, h = std.text.mensure(text)
     std.text.print((data.width/2) - (w/2), (data.height/2) - h, text)
     std.text.font_name('Plex')
+    std.text.font_size(18)
+    local btn = 'Home'
+    local w, h = std.text.mensure(btn)
+    std.text.print((data.width/2) - (w/2), (data.height/2) + 50,btn)
 end
 
 return Wind
