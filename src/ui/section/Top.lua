07cb78be570bc4e4cc143f81c6e0e37dae56d5fa
João Vicente <joao.vicente@zedia.com.br>
2025-05-22 18:15:53 -0300

    refactor: no infos in top

--- src/ui/section/Top.lua
+++ src/ui/section/Top.lua
@@ -1,22 +1,12 @@
 local Top = {}
 
 function Top.load(std, data)
-    data.ui_grid = std.ui.grid('5x2')
-        :margin(50)
-        :add(std.node.load('src/ui/elements/TextOutline.lua'), 5)
-        :add(std.node.load('src/ui/widgets/BigDay.lua'))
-        :apply()
-
-    data.city = data.ui_grid:get_item(1)
-    data.city.data.listen = 'put_gelocation_str'
-    data.city.data.padding = 8
-
-    data.ui_grid:get_item(2).data.labels = {
-        os.date("%d"),
-        os.date("%B"),
-        os.date("%H:%M"),
-        os.date("%A")
-    }
+    -- local grid = std.ui.grid('14x1')
+    --     :add({},11)
+    --     :add(std.node.load('src/ui/section/MenuInfos.lua'), 3)
+    --     :apply()
+    -- local info = grid:get_item(2)
+    -- info.data.background = true
 end
 
 return Top
