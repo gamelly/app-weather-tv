97e2867481baadf09008c7030b212dbb8d24b6c9
João Vicente <joao.vicente@zedia.com.br>
2025-05-22 18:18:39 -0300

    feat: add gradient background, adjust button font size, and update background color

--- src/ui/section/Menu.lua
+++ src/ui/section/Menu.lua
@@ -8,17 +8,17 @@ function Options.load(std, data)
         :add(std.node.load('src/ui/components/Button.lua'))
         :apply()
 
-    data.ui_grid:get_item(1).data.label = 'Hour'
-    data.ui_grid:get_item(1).data.size = 12
-    data.ui_grid:get_item(2).data.label = 'Day'
-    data.ui_grid:get_item(2).data.size = 12
+    data.ui_grid:get_item(1).data.label = 'Hora'
+    data.ui_grid:get_item(1).data.size = 20
+    data.ui_grid:get_item(2).data.label = 'Dia'
+    data.ui_grid:get_item(2).data.size = 20
 end
 
 function Menu.load(std, data)
     local index = 1
 
     local grid = std.ui.grid('14x1')
-        :add({})
+        :add(std.node.load('src/ui/widgets/Wind.lua'))
         :add(Options, 2)
         :add(std.node.load('src/ui/widgets/Calendar.lua'))
         :add(std.node.load('src/ui/widgets/Calendar.lua'))
@@ -28,9 +28,12 @@ function Menu.load(std, data)
         :add(std.node.load('src/ui/widgets/Calendar.lua'))
         :add(std.node.load('src/ui/widgets/Calendar.lua'))
         :add(std.node.load('src/ui/widgets/Calendar.lua'))
-        :add(std.node.load('src/ui/widgets/Wind.lua'), 2)
+        :add(std.node.load('src/ui/section/CurrentTemp.lua'), 3)
         :apply()
 
+    grid:get_item(11).data.label = 'Nesse momento'
+    grid:get_item(11).data.size = 26
+    
     while index <= 8 do
         grid:get_item(index + 2).data.pattern = string.format('put_%d_%%d_calendar', index)
         index = index + 1
@@ -39,8 +42,9 @@ function Menu.load(std, data)
     grid:get_item(3).data.inverse = true
 end
 function Menu.draw(std, data)
-    std.draw.color(0x333333AA)
-    std.draw.rect(0, 0, 0, data.width, data.height)
+    std.draw.color(0x2C292EFF)
+    std.draw.rect(0, 0, 40, data.width, data.height)
+    std.draw.image('assets/opening_background.png',0,-294)
 end
 
 return Menu
