6c72bd22a963a1944e96d33d702b1b261b639f5f
João Vicente <joao.vicente@zedia.com.br>
2025-05-23 14:07:05 -0300

    refactor: standardize double quotes (") to single quotes (')

--- src/ui/section/MenuInfos.lua
+++ src/ui/section/MenuInfos.lua
@@ -1,11 +1,11 @@
 local MenuInfos = {}
 local months_pt_br = {
-    "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
-    "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
 }
 local day_of_week_pt_br = {
-    "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira",
-    "Quinta-feira", "Sexta-feira", "Sábado"
+    'Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira',
+    'Quinta-feira', 'Sexta-feira', 'Sábado'
 }
 
 function MenuInfos.load(std, data)
